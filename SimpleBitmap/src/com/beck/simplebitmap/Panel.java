1807f675c558df45a560d04e06a30c38440bc1c4
root <root@localhost>
2012-12-11 11:34:42 -0600

    Initial Lesson.

--- SimpleBitmap/src/com/beck/simplebitmap/Panel.java
+++ SimpleBitmap/src/com/beck/simplebitmap/Panel.java
@@ -1,9 +1,11 @@
 package com.beck.simplebitmap;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -11,14 +13,32 @@ import android.view.SurfaceView;
 
 
 public class Panel extends SurfaceView implements SurfaceHolder.Callback{
+	//GLOBAL VARIABLES
 	private CanvasThread canvasthread;
+    //Variable used to hold our bitmap
+	Bitmap bm;
+    //Variable used to hold the current rotation value of the bitmap
+    private int rotation = 0;
+    //A Matrix is used to define size, position, and rotation 
+    //of a graphic. We can set up a matrix and use it when drawing.
+    private Matrix position; 
+    //x and y hold the width and height of our canvas.
+    int x;
+    int y;
+    //END GLOBAL VARIABLES
+    
     
     public Panel(Context context, AttributeSet attrs) {
 		super(context, attrs); 
 		// TODO Auto-generated constructor stub
-	    getHolder().addCallback(this);
+	   
+		getHolder().addCallback(this);
 	    canvasthread = new CanvasThread(getHolder(), this);
 	    setFocusable(true);
+	    //Here we load our Bitmap variable with the graphic ship.png in the drawable-hdpi folder.
+	    bm = BitmapFactory.decodeResource(getResources(), R.drawable.ship);
+	    //Here we initialize our position Matrix.
+        position = new Matrix();
 	 
 	}
 
@@ -36,12 +56,52 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback{
 
 	@Override
 	public void onDraw(Canvas canvas) {
-	
+       //x and y are **global** variables defined at the top of this class. We need to know how big our screen
+	   // is. The center on a phone will be different than the center point on a
+	   // tablet.
 		
+		x = canvas.getWidth();
+        y = canvas.getHeight();			   
+	   
+        update();	
+        canvas.drawColor(Color.BLACK);
 		
+        //The Matrix position is updated within the update() method.
+	    
+	    canvas.drawBitmap(bm,position, null);
+		
+	   
+	  
 		
 	}
 
+	 public void update() {
+	      
+	       //Matrix m is a **local** variable. We will configure m and use it
+		   // set the value of our global Matrix position.
+		   Matrix m = new Matrix();
+	       
+		   //Current rotation value
+		   m.postRotate(rotation, bm.getWidth()/2, bm.getHeight()/2);
+	       
+		   //This is where we define the center of the screen.
+		   //x = canvas.getWidth();   --Look in onDraw
+		   //y = canvas.getHeight();  --Look in on Draw
+		   //Example: If the canvas width is 10, then x/2 would equal 5, --the horizontal center.
+		   //We use positionx and positiony to draw the center of the bitmap to the center of the screen.
+		   
+		   int positionx = ((x/2) - bm.getWidth()/2);
+	       int positiony = ((y/2) - bm.getHeight()/2);
+	       
+	       m.postTranslate(positionx, positiony);
+
+	        // This method sets the value of of our **global** variable (position) to the **local** variable (m)
+	        position.set(m);
+
+	        //update the rotation value. 
+	        //We call update every frame refresh in onDraw.
+	        rotation += 2;
+	    }
 
 
 
